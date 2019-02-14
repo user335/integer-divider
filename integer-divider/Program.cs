cb1106190098036c9e4c9cdd37fa5f2410adf470
Andy Weekes <userthreethreefive@gmail.com>
2019-02-14 13:21:26 -0700

    finally catching divide by zero exceptions

--- integer-divider/Program.cs
+++ integer-divider/Program.cs
@@ -13,11 +13,11 @@ namespace integer_divider
             List<int> intlist = new List<int>()
             {
                 1, 2, 3, 4, 5, 6,
-                2, 4, 6, 8, 10, 12
+                2, 4, 6, 8, 10, -12
             };
             float divisor = -1;
-
-            while (divisor <= 0) PromptForDivision();
+            bool succeeded = false;
+            while (!succeeded) PromptForDivision();
 
             void PromptForDivision()
             {
@@ -26,6 +26,27 @@ namespace integer_divider
                 try
                 {
                     divisor = Convert.ToSingle(answer);
+                    Console.WriteLine("Here are your results: ");
+
+                    //this will be caught by dividebyzeroexception. 
+                    //Required because the block that's actually expected to work does not produce exceptions when dividing by zero
+                    //no clue why that is but if you don't believe me comment out the following 7 lines and run the program with 0 for input. everything returns infinity symbols
+                    if (divisor == 0) 
+                    {
+                        for (int i = 0; i < intlist.Count; i++)
+                        {
+                            Console.WriteLine(intlist[i] + " / 0 = " + intlist[i] / 0);
+                        }
+                    }
+
+                    //this will NOT be caught by dividebyzeroexception even when dividing by zero! console writes infinity symbols for every result
+                    for (int i = 0; i < intlist.Count; i++)
+                    {                        
+                        Console.WriteLine(intlist[i] / divisor);  
+                    }
+
+                    Console.WriteLine("Everything seems fine.");
+                    succeeded = true;
                 }
                 catch (FormatException)
                 {
@@ -35,21 +56,18 @@ namespace integer_divider
                 {
                     Console.WriteLine("Argument Exception detected. I blame you.");
                 }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Are you trying to make me divide by zero? Not cool, bro!");
+                }
                 catch (Exception ex)
                 {
                     Console.WriteLine("I have no idea what you did. Error message = " + ex);
                 }
-                if (divisor == 0) Console.WriteLine("Are you trying to make me divide by zero? Not cool, bro!");
-                if (divisor <= 0) Console.WriteLine("Let's try that again...");
+                //if (divisor == 0) Console.WriteLine("Are you trying to make me divide by zero? Not cool, bro!");
+                if (!succeeded) Console.WriteLine("Let's try that again...");
             }
 
-            //5
-            Console.WriteLine("Seems like you entered a good value. Thanks, that makes my job easier.");
-            Console.WriteLine("Here are your results: ");
-            for (int i = 0; i < intlist.Count; i++)
-            {
-                Console.WriteLine(intlist[i] / divisor);
-            }
 
             Console.Read();
         }
